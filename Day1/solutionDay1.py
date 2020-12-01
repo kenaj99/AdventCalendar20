50afa9b1a2440e15b6355b7c49f450475d2a7b6a
kenaj99 <banot.jan@gmail.com>
2020-12-01 18:59:56 +0100

    CHG: Added solution to Day1 Part2 and added small changes to Day1 Part1

--- Day1/solutionDay1.py
+++ Day1/solutionDay1.py
@@ -1,17 +1,31 @@
-def readFile (filename):
-    fileObject = open(filename, "r") #file is opened in read mode
-    entries = fileObject.read().splitlines() #puts the file content into array
+def readFile(filename):
+    fileObject = open(filename, "r")  # file is opened in read mode
+    entries = fileObject.read().splitlines()  # puts the file content into array
     fileObject.close()
     return entries
+
+
 inputFile = "inputDay1.txt"
 
 expensesArray = readFile(inputFile)
-result = 0
+result1 = 0
 
 for element in expensesArray:
     a = int(element)
     b = 2020 - a
     if str(b) in expensesArray:
-        result = a * b
-        
-print(result)
+        result1 = a * b
+
+print("Result 1: " + str(result1))
+
+for element in expensesArray:
+    for secondElement in expensesArray:
+        a = int(element)
+        b = int(secondElement)
+        if a + b < 2020:
+            c = 2020 - (a + b)
+            if str(c) in expensesArray:
+                result2 = a * b * c
+
+print("Result 2: " + str(result2))
+
